0484c9c5799c6ed08e57a14b555df0bb1da6c8cb
jkroso <jkroso@gmail.com>
2013-04-25 20:00:23 +1200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,38 @@
+
+# untar
+
+  A simple tar file unpacker. tar files are a bit of a box of chocolates in that you never know what the top level directory within them is going to be called or how many excess directories there might be. This function takes care of that problem by chopping off all unnecessary path segments.
+
+## Getting Started
+
+_With npm_  
+
+	$ npm install untar --save
+
+then in your app:
+
+```js
+var untar = require('untar')
+```
+
+## API
+
+- [untar()](#untar)
+
+### untar(String, Stream)
+
+The untar function takes a path to a target directory and a stream for a tar file. It returns a promise which will resolve on completion. 
+
+## Running the tests
+
+```bash
+$ npm install
+$ node examples/index.js
+```
+## todo
+
+add a separate function for handling buffered tars
+
+## License 
+
+[MIT](License)
---
+++ index.js
@@ -0,0 +1,46 @@
+
+var common = require('path/common')
+  , join = require('path/join')
+  , Promise = require('laissez-faire/full')
+  , parse = require('tar-parse')
+  , each = require('foreach/async')
+  , write = require('writefile')
+
+/**
+ * Place the contents of a tar stream into the 
+ * `dest` directory
+ * (String dir, Stream tar) -> Promise nil
+ */
+
+module.exports = function(dest, pkg){
+	return unpack(pkg).then(function(files){
+		var base = common(Object.keys(files))
+			.replace(/^\//, '')
+			.replace('/', '\\/')
+		var regx = new RegExp('^\\/?'+base+'\\/?')
+		return each(files, function(stream, path, done){
+			path = join(dest, path.replace(regx, ''))
+			write(path, stream).node(done)
+			stream.resume()
+		})
+	})
+}
+
+/**
+ * unpack the contents `pkg` into an Object
+ * (Stream) -> Promise object
+ */
+
+function unpack(pkg){
+	var p = new Promise
+	var files = {}
+	pkg.pipe(parse())
+		.on('data', function(entry){
+			if (entry.type != 'File') return
+			files[entry.path] = entry
+			entry.pause()
+		})
+		.on('error', function(e){ p.reject(e) })
+		.on('end', function(){ p.fulfill(files) })
+	return p
+}
